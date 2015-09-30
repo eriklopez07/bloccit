edb3ab96609aee86a9dcf5c24c8600cb71540f4c
Erik Lopez <eriklopez0712@gmail.com>
2015-09-30 19:57:53 -0400

    Assignment-40-Changes2

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
       resources :sponsored_posts, except: [:index]
     end
 
-    resources :users, only: [:new, :create]
+    resources :users, only: [:new, :create] do
+      resources :confirm
+    end  
 
 
 get 'about' => 'welcome#about'
