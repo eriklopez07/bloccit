43ecd218ad8c7622e1b1bca3f5d2e42390a1813e
Erik Lopez <eriklopez0712@gmail.com>
2015-10-09 16:46:26 -0400

    Database fix

--- db/migrate/20150908170733_add_question_to_answers.rb
+++
@@ -1,6 +0,0 @@
-class AddQuestionToAnswers < ActiveRecord::Migration
-  def change
-    add_reference :answers, :question, index: true
-    add_foreign_key :answers, :questions
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -23,12 +23,9 @@ ActiveRecord::Schema.define(version: 20151007210514) do
 
   create_table "answers", force: :cascade do |t|
     t.text     "body"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.integer  "question_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
-
-  add_index "answers", ["question_id"], name: "index_answers_on_question_id"
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
@@ -52,14 +49,6 @@ ActiveRecord::Schema.define(version: 20151007210514) do
 
   add_index "posts", ["topic_id"], name: "index_posts_on_topic_id"
   add_index "posts", ["user_id"], name: "index_posts_on_user_id"
-
-  create_table "questions", force: :cascade do |t|
-    t.string   "title"
-    t.text     "body"
-    t.boolean  "resolved"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
   create_table "sponsored_posts", force: :cascade do |t|
     t.string   "title"
