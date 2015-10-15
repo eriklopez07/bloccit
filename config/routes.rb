55eeb843ba030d692e74c205edc79a1184376b80
Erik Lopez <eriklopez0712@gmail.com>
2015-10-15 17:51:38 -0400

    Labels

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,24 @@
 Rails.application.routes.draw do
 
-  get 'labels/show'
 
 
-    resources :topics do
-      resources :posts, except: [:index]
-      resources :sponsored_posts, except: [:index]
-    end
+  resources :topics do
+    resources :posts, except: [:index]
+    resources :sponsored_posts, except: [:index]
+  end
 
-    resources :users, only: [:new, :create, :show]
-   post 'users/confirm' => 'users#confirm'
+  resources :users, only: [:new, :create, :show]
+  post 'users/confirm' => 'users#confirm'
 
   resources :sessions, only: [:new, :create, :destroy]
 
   resources :posts, only: [] do
     resources :comments, only: [:create, :destroy]
   end
+  resources :labels, only: [:show]
 
 
-get 'about' => 'welcome#about'
+  get 'about' => 'welcome#about'
 
   root to: 'welcome#index'
 
