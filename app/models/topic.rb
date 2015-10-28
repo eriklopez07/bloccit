d9d9a111720e99ee0a1bc796998015226d69ef07
Erik Lopez <eriklopez0712@gmail.com>
2015-10-28 13:40:02 -0400

    A-46

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -3,8 +3,10 @@ class Topic < ActiveRecord::Base
   has_many :labelings, as: :labelable
   has_many :labels, through: :labelings
   has_many :sponsored_posts, dependent: :destroy
-
+  scope :publically_viewable, -> { where(public: true) }
+  scope :privately_viewable, -> { where(public: false) }
   scope :visible_to, -> { where(public: true) }
+  scope :visable_to, -> (user) {user ? all : publically_viewable}
 
   validates :name, length: { minimum: 5 }, presence: true
   validates :description, length: { minimum: 15 }, presence: true
